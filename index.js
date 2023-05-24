4ecade36464f5d368e4523e77a54feccb80fd135
MatheusCBarreto <devmattbarreto@protonmail.com>
2023-05-24 16:19:21 -0300

    Conclusão do front e agora é só conectar o db com o node

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -11,6 +16,13 @@ app.get('/', (req, res) => {
 app.get("/perguntar", (req, res) => {
   res.render("perguntar");
 })
+
+app.post("/salvarperguntas", (req, res) => {
+  let titulo = req.body.titulo;
+  let descricao = req.body.descricao;
+  res.send("Formulário recebido!");
+});
+
 
 app.listen(8080, () => {
   console.log('Servidor iniciado com sucesso!');
