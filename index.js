3e7b120d66c1f3d9915a65b267bf1acbf79c9a4c
MatheusCBarreto <devmattbarreto@protonmail.com>
2023-05-23 21:12:12 -0300

    primeiros passos com bootstrap (instalação)

--- index.js
+++ index.js
@@ -2,18 +2,10 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
-  let nome = 'Matheus';
-  let lang = 'JavaScript';
-  let exibirMsg = false;
-  res.render('index', {
-    nome: nome,
-    lang: lang,
-    empresa: 'Guia do programador',
-    inscritos: 8000,
-    msg: exibirMsg,
-  });
+  res.render('index');
 });
 
 app.listen(8080, () => {
